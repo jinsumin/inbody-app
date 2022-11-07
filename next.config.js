5e393b53d3c78ab3795124add961e6672e8efe07
jinsumin <suminjin@naver.com>
2022-11-07 22:29:07 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,21 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  swcMinify: true,
+  reactStrictMode: true,
+  // swcMinify: true,
   images: {
     domains: ["www.inbody.co.kr"],
   },
 };
 
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://inbody-board.vercel.app/:path*",
-      },
-    ];
-  },
-};
+// module.exports = {
+//   async rewrites() {
+//     return [
+//       {
+//         source: "/api/:path*",
+//         destination: "https://inbody-board.vercel.app/:path*",
+//       },
+//     ];
+//   },
+// };
 
 module.exports = nextConfig;
