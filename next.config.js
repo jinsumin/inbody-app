72ca7eeb355313d1b651ca77517caa6f53b03b3e
jinsumin <suminjin@naver.com>
2022-11-09 00:22:05 +0900

    cors problem

--- next.config.js
+++ next.config.js
@@ -1,21 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   // swcMinify: true,
   images: {
     domains: ["www.inbody.co.kr"],
   },
 };
 
-// module.exports = {
-//   async rewrites() {
-//     return [
-//       {
-//         source: "/api/:path*",
-//         destination: "https://inbody-board.vercel.app/:path*",
-//       },
-//     ];
-//   },
-// };
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://inbody-board.vercel.app/:path*",
+      },
+    ];
+  },
+};
+
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://api.notion.com/:path*'
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig;
